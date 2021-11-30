4b47748a4c86b25eb1c27a5299bccbec495f316e
Lucas Moreno <88151807+trikteed@users.noreply.github.com>
2021-11-30 19:36:39 -0300

    Create Relogio.js

---
+++ Relogio.js
@@ -0,0 +1,92 @@
+<table cellspacing="5px">
+<tr class="hrow">
+<td>GMT</td><td>Vancouver</td><td>San Francisco</td><td>Seattle</td>
+</tr>
+<tr>
+<td><span id="GMT"></span></td><td><span id="Vancouver"></span></td><td><span id="SanFrancisco"></span></td><td><span id="Seattle"></span></td>
+</tr>
+<tr class="hrow">
+<td>Los Angeles</td><td>Denver</td><td>Mexico City</td><td>Houston</td>
+</tr>
+<tr>
+<td><span id="LosAngeles"></span></td><td><span id="Denver"></span></td><td><span id="MexicoCity"></span></td><td><span id="Houston"></span></td>
+</tr>
+<tr class="hrow">
+<td>Minneapolis</td><td>New Orleans</td><td>Chicago</td><td>Montgomery</td>
+</tr>
+<tr>
+<td><span id="Minneapolis"></span></td><td><span id="NewOrleans"></span></td><td><span id="Chicago"></span></td><td><span id="Montgomery"></span></td>
+</tr>
+<tr class="hrow">
+<td>Indianapolis</td><td>Atlanta</td><td>Detroit</td><td>Miami</td>
+</tr>
+<tr>
+<td><span id="Indianapolis"></span></td><td><span id="Atlanta"></span></td><td><span id="Detroit"></span></td><td><span id="Miami"></span></td>
+</tr>
+<tr class="hrow">
+<td>Washington DC</td><td>Philadelphia</td><td>New York</td><td>Montreal</td>
+</tr>
+<tr>
+<td><span id="WashingtonDC"></span></td><td><span id="Philadelphia"></span></td><td><span id="NewYork"></span></td><td><span id="Montreal"></span></td>
+</tr>
+<tr class="hrow">
+<td>Boston</td><td>Buenos Aires</td><td>Sao Paulo</td><td>Rio De Janeiro</td>
+</tr>
+<tr>
+<td><span id="Boston"></span></td><td><span id="BuenosAires"></span></td><td><span id="SaoPaulo"></span></td><td><span id="RioDeJaneiro"></span></td>
+</tr>
+<tr class="hrow">
+<td>Lisbon</td><td>Dublin</td><td>London</td><td>Madrid</td>
+</tr>
+<tr>
+<td><span id="Lisbon"></span></td><td><span id="Dublin"></span></td><td><span id="London"></span></td><td><span id="Madrid"></span></td>
+</tr>
+<tr class="hrow">
+<td>Barcelona</td><td>Paris</td><td>Brussels</td><td>Amsterdam</td>
+</tr>
+<tr>
+<td><span id="Barcelona"></span></td><td><span id="Paris"></span></td><td><span id="Brussels"></span></td><td><span id="Amsterdam"></span></td>
+</tr>
+<tr class="hrow">
+<td>Frankfurt</td><td>Rome</td><td>Berlin</td><td>Prague</td>
+</tr>
+<tr>
+<td><span id="Frankfurt"></span></td><td><span id="Rome"></span></td><td><span id="Berlin"></span></td><td><span id="Prague"></span></td>
+</tr>
+<tr class="hrow">
+<td>Vienna</td><td>Stockholm</td><td>Athens</td><td>Helsinki</td>
+</tr>
+<tr>
+<td><span id="Vienna"></span></td><td><span id="Stockholm"></span></td><td><span id="Athens"></span></td><td><span id="Helsinki"></span></td>
+</tr>
+<tr class="hrow">
+<td>Minsk</td><td>Istanbul</td><td>Cairo</td><td>Jerusalem</td>
+</tr>
+<tr>
+<td><span id="Minsk"></span></td><td><span id="Istanbul"></span></td><td><span id="Cairo"></span></td><td><span id="Jerusalem"></span></td>
+</tr>
+<tr class="hrow">
+<td>Beirut</td><td>Moscow</td><td>Baghdad</td><td>Dubai</td>
+</tr>
+<tr>
+<td><span id="Beirut"></span></td><td><span id="Moscow"></span></td><td><span id="Baghdad"></span></td><td><span id="Dubai"></span></td>
+</tr>
+<tr class="hrow">
+<td>Bangkok</td><td>Jakarta</td><td>Hong Kong</td><td>Beijing</td>
+</tr>
+<tr>
+<td><span id="Bangkok"></span></td><td><span id="Jakarta"></span></td><td><span id="HongKong"></span></td><td><span id="Beijing"></span></td>
+</tr>
+<tr class="hrow">
+<td>Shanghai</td><td>Seoul</td><td>Tokyo</td><td>Melbourne</td>
+</tr>
+<tr>
+<td><span id="Shanghai"></span></td><td><span id="Seoul"></span></td><td><span id="Tokyo"></span></td><td><span id="Melbourne"></span></td>
+</tr>
+<tr class="hrow">
+<td>Sydney</td><td>Brisbane</td><td>Vladivostok</td><td>Kamchatka</td>
+</tr>
+<tr>
+<td><span id="Sydney"></span></td><td><span id="Brisbane"></span></td><td><span id="Vladivostok"></span></td><td><span id="Kamchatka"></span></td>
+</tr>
+</table>
